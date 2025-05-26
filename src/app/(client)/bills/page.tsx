65a4e5f8436829a2f9ad37ab10610f6fe8878fe2
abhishek-nirala <doctordeath918@gmail.com>
2025-05-26 23:17:23 +0530

    some minor fix

--- src/app/(client)/bills/page.tsx
+++ src/app/(client)/bills/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Bills() {
   return (
-    <div>Bills</div>
+    <div>
+<h1 className="tex-xl font-medium">Page is under construction</h1>
+
+
+    </div>
   )
 }
 
